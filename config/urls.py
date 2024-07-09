cc28806fe30e6b5ab947058dc280ca45c626706e
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2024-07-09 12:26:50 +0500

    csrf token remove

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ schema_view = get_schema_view(
       license=openapi.License(name="Hozirda mavjud emas!"),
    ),
    public=True,
-   permission_classes=[permissions.IsAuthenticated,],
+   permission_classes=[permissions.AllowAny,],
 )
 
 urlpatterns = [

3d11938e73f0cc244a7136c6b29b025e711206e3
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2024-07-09 15:48:35 +0500

    csrf token add

--- config/settings.py
+++ config/settings.py
@@ -47,8 +47,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ schema_view = get_schema_view(
       license=openapi.License(name="Hozirda mavjud emas!"),
    ),
    public=True,
-   permission_classes=[permissions.AllowAny,],
+   permission_classes=[permissions.IsAuthenticated,],
 )
 
 urlpatterns = [
